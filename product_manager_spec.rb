c6f4fd83961d723d4ff2e56ac6e8ef29e0e66bc7
Juan Bonfante <juan.bonfante@gmail.com>
2019-03-18 21:35:02 -0500

    [Red] Added initial tests to add products to product manager

--- product_manager_spec.rb
+++ product_manager_spec.rb
@@ -4,8 +4,18 @@ require './product_manager'
 describe ProductManager do
 
   it 'should have a list of products' do
-
     expect(subject.products).to be_a(Array)
+  end
+
+  context 'adding products' do
+    it 'should respond to add_product method' do
+      expect(subject).to respond_to(:add_product)
+    end
+
+    it 'should add a product' do
+      product_info = {name:'test', price: 0.99, by_weight: true, unit: 'lb'}
+      expect{ subject.add_product(product_info) }.to change(subject.products.count).from(0).to(1)
+    end
   end
 end
 
